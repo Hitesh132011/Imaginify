ea27d14383a0a056ce4d4c09bc1582b07e092d3f
Hitesh Mohanty <hiteshmohanty33@gmail.com>
2024-08-03 00:43:57 +0530

    lat

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,20 +1,20 @@
 /* eslint-disable camelcase */
 import { clerkClient, getAuth, buildClerkProps } from "@clerk/nextjs/server";
-import { GetServerSideProps } from "next";import { WebhookEvent } from "@clerk/nextjs/server";
-import { headers } from "next/headers";
+import { GetServerSideProps } from "next";
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
 import { NextResponse } from "next/server";
-import { Webhook } from "svix";
 
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
   // Get the headers
@@ -25,19 +25,19 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
   // Get the body
-  const payload = await req.json();
+  const payload = await req.json()
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+  let evt: WebhookEvent
 
   // Verify the payload with the headers
   try {
@@ -45,12 +45,12 @@ export async function POST(req: Request) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    }) as WebhookEvent;
+    }) as WebhookEvent
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    })
   }
 
   // Get the ID and type
