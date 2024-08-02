09fdf3347f7f309b99977fb84b8453f86d76b709
Hitesh Mohanty <hiteshmohanty33@gmail.com>
2024-08-02 13:23:15 +0530

    intial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <MobileNav/>
       <div className="root-container">
         <div className="wrapper">
-          {children}
+        
         </div>
       </div>
     </main>
